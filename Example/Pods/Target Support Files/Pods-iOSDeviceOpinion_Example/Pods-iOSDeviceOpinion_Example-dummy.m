6625b2ec5bddb59d3502dadcd33b4b68476a4cae
tengteng <1107326411@qq.com>
2022-03-19 17:29:33 +0800

    1.0.0

---
+++ Example/Pods/Target Support Files/Pods-iOSDeviceOpinion_Example/Pods-iOSDeviceOpinion_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSDeviceOpinion_Example : NSObject
+@end
+@implementation PodsDummy_Pods_iOSDeviceOpinion_Example
+@end
---
+++ Example/Pods/Target Support Files/iOSDeviceOpinion/iOSDeviceOpinion-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSDeviceOpinion : NSObject
+@end
+@implementation PodsDummy_iOSDeviceOpinion
+@end
